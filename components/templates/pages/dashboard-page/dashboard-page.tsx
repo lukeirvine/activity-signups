0c5743233e65ac2852a2fa1a73229429989cb0ea
lukeirvine <lukeirvinefreelance@gmail.com>
2024-08-12 23:28:10 -0700

    dashboard route redirects to auth if not authenticated

--- components/templates/pages/dashboard-page/dashboard-page.tsx
+++ components/templates/pages/dashboard-page/dashboard-page.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react";
+import { useRouter } from "next/navigation";
 import SideNav from "@/components/organisms/nav/side-nav/side-nav";
 import PagePadding from "@/components/atoms/containers/page-padding/page-padding";
+import { useCurrentUser } from "@/hooks/use-user";
 
 type DashboardPageProps = {
   children: ReactNode;
@@ -9,12 +11,28 @@ type DashboardPageProps = {
 const DashboardPage: React.FC<Readonly<DashboardPageProps>> = ({
   children,
 }) => {
+  const user = useCurrentUser();
+  const router = useRouter();
+
+  if (user === undefined) {
+    router.push("/auth/login");
+  }
+
   return (
     <PagePadding>
-      <div className="fixed top-0 left-0 h-screen pt-16">
-        <SideNav />
-      </div>
-      <div className="h-full pl-56">{children}</div>
+      {user === null && (
+        <div className="w-screen h-full flex justify-center items center">
+          <div className="loading loading-dots loading-lg mb-40"></div>
+        </div>
+      )}
+      {user && (
+        <div>
+          <div className="fixed top-0 left-0 h-screen pt-16">
+            <SideNav />
+          </div>
+          <div className="h-full pl-56">{children}</div>
+        </div>
+      )}
     </PagePadding>
   );
 };
