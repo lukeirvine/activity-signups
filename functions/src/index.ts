094409a3237f8001e0ebef199efe84ee9adc0076
lukeirvine <lukeirvinefreelance@gmail.com>
2024-08-20 21:57:46 -0700

    added boiler plate typescript cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+// import functions = require("firebase-functions");
+import {QueryDocumentSnapshot} from "firebase-admin/firestore";
+import {logger} from "firebase-functions";
+import {onDocumentDeleted} from "firebase-functions/v2/firestore";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +21,16 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.deleteChildren = onDocumentDeleted("weeks/{weekId}", async (event) => {
+  // The document snapshot of the deleted document
+  const document: QueryDocumentSnapshot | undefined = event.data;
+
+  if (!document?.exists) {
+    logger.error("Document does not exist.");
+    return;
+  }
+
+  console.log("Document deleted", document.id);
+  return;
+});
