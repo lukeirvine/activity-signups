8aa1aed9d05e5815a08fc050fc8f33d705c00a84
lukeirvine <lukeirvinefreelance@gmail.com>
2024-08-22 10:27:54 -0700

    testing script

--- scripts/commit.js
+++ scripts/commit.js
@@ -9,7 +9,15 @@ if (!commitMessage) {
 }
 
 try {
+  // Run lint-fix and stop the script if it fails
   execSync('npm run lint-fix', { stdio: 'inherit' });
+} catch (error) {
+  console.error('Lint fix failed. Aborting commit.');
+  process.exit(1);
+}
+
+try {
+  // Proceed with the rest of the steps if lint-fix succeeds
   execSync('git add --all', { stdio: 'inherit' });
   execSync(`git commit -m "${commitMessage}"`, { stdio: 'inherit' });
   execSync('git push', { stdio: 'inherit' });
