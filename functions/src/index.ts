9d455d3f90a6d2168a42fa044b03e53c26d64dd5
lukeirvine <lukeirvinefreelance@gmail.com>
2024-11-30 21:49:58 -0800

    removed database transform function definition

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,8 +20,7 @@ import {
   cleanOnDepartmentDelete,
   deleteWeeksChildren,
 } from "./firestore";
-import {getAllDocs, updateDoc, verifyPermissions} from "./utils";
-import {Activity} from "./firebase-types";
+import {throwError, verifyPermissions} from "./utils";
 
 // firestore.ts
 exports.deleteWeeksChildren = deleteWeeksChildren;
@@ -31,23 +30,5 @@ exports.changeDayDatesOnWeekChange = changeDayDatesOnWeekChange;
 
 exports.databaseTransform = onCall(async (request) => {
   verifyPermissions(request);
-  console.log("database-transform function called with data");
-  // Get all docs from activities collection and add a activitySetId value to them
-  // id: d4e9fc43-f3c4-42a0-be1d-be6c255c59fe
-  try {
-    const activities = await getAllDocs<Activity>("activities");
-    for (const activity of Object.values(activities)) {
-      if (activity.id) {
-        await updateDoc<Activity>({
-          collectionId: "activities",
-          docId: activity.id,
-          data: {activitySetId: "d4e9fc43-f3c4-42a0-be1d-be6c255c59fe"},
-        });
-      }
-    }
-  } catch (error) {
-    console.error("Error updating activities: ", error);
-    return {message: "An error occurred"};
-  }
-  return {message: "Transformation complete"};
+  throwError("unimplemented", "Not yet implemented");
 });
